5f3dae0febb79797eb21f70e89605f9e72ce2fb8
Emerald Oriloye <oluwapelumibalogun2@gmail.com>
2023-07-19 03:42:36 -0700

    push project

---
+++ JS Beginners assignment.js
@@ -0,0 +1,56 @@
+
+
+ 1. // comments can be made readable
+
+ 2. // Welcome to 30DaysOfJavaScript
+
+ 3. /* comments can make code readable, easy to reuse and informative */
+   
+   
+   4.
+var str = 'I am a string';
+var a = true;
+var b = false;
+var c = undefined;
+var d = null;
+
+
+    5.
+
+console.log(typeof str);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+
+
+     6.
+let firstVal;
+var seconVar;
+var thirdVal;
+let fourthVal;
+
+     7.
+var num = 51;
+var arr = ['Benson', 'Grace', 'Edet', 'Naomi', 'June'];
+let obj = {name: 'Dave Kilt', age: 36, occupation: 'Software Engineer', happy: true}
+const val = 90;
+
+
+     8.
+const firstName = 'Emmanuel';
+const lastName = 'Balogun';
+const maritalStatus = 'single';
+const country = 'Nigeria';
+const age = 48;
+
+     9.
+ let firstname = 'Emmanuel', lastname = 'Balogun', maritalstatus = "single", mycountry = 'Nigeria', myage = 48;
+
+       
+     10.
+ var myAge = 48;
+ var yourAge = 30;
+ console.log(myAge);
+ console.log(yourAge);
