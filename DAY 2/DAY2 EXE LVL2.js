7a1e12a0c605307f2546e2066b96a28bb0d425e3
Emerald Oriloye <oluwapelumibalogun2@gmail.com>
2023-08-07 09:36:34 -0700

    added some projects

--- DAY 2/DAY2 EXE LVL2.js
+++ DAY 2/DAY2 EXE LVL2.js
@@ -1,4 +1,3 @@
-     1.
 let wiseQuote = `The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`
 console.log(wiseQuote);
 
---
+++ DAY 3/EXE LV1.js
@@ -0,0 +1,34 @@
+
+const firstName = 'Emerald'
+const lastName =  'Oriloye'
+const country  =  'Zambia'
+const city = 'Lithuso'
+const age = 28
+const isMarried = false
+const year = 2096
+
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(country));
+console.log(typeof(city));
+console.log(typeof(age));
+console.log(typeof(isMarried));
+console.log(typeof(year));
+
+let num1 = 10
+let str1 = '10';
+console.log(typeof(str1) === typeof(num1)); //false
+
+let num2 = '9.8';
+let newNum2 = parseInt(num2);
+console.log(newNum2 == num1); // false
+
+const name = 'James';
+const num3 = 911
+const loveFood = true;
+
+const anon = '';
+const empty = null;
+const likeBook = false;
+
+console.log(4 > 3);
